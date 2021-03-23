c0d7da9bb3793e9301063501531a24af80cad65c
Samy Ouadhi <samyouadhi@gmail.com>
2021-03-24 00:01:30 +0100

    add camera animation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
+    <button class="test-button" @click="updateCamera">ZoomIn</button>
     <Scene />
   </div>
 </template>
 
 <script>
-import Scene from "./components/Scene"
+import Scene from "./components/Scene";
 
 export default {
   name: "App",
   components: {
-    Scene
+    Scene,
+  },
+  methods: {
+    updateCamera() {
+      this.$store.commit("updateCamera", {posX: 0.5, posY: -1, posZ: 1.25});
+    },
   },
 };
 </script>
 
 <style lang="scss">
 html,
-body {   
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
 }
 
 .container {
-    position: fixed;
-    top: 0;
-    left: 0;
-    outline: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  outline: none;
+}
+
+.test-button {
+  position: absolute;
+  top: 24px;
+  left: 24px;
 }
 </style>
--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -3,7 +3,10 @@
 </template>
 
 <script>
-import * as Three from "three";
+import { mapState } from "vuex";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { gsap } from "gsap";
 
 export default {
   name: "Scene",
@@ -11,6 +14,7 @@ export default {
     return {
       camera: null,
       scene: null,
+      controls: null,
       renderer: null,
       mesh: null,
       sizes: {
@@ -20,6 +24,24 @@ export default {
       aspect: null,
     };
   },
+  computed: mapState(["cameraPosition"]),
+  watch: {
+    cameraPosition(value) {
+      console.log("Update", value);
+      let camera = this.camera;
+      let center = new THREE.Vector3(0, 0, 0)
+      gsap.to(this.camera.position, {
+        x: value.posX,
+        y: value.posY,
+        z: value.posZ,
+        duration: 1,
+        ease: "power3.inOut",
+        onUpdate() {
+          camera.lookAt(center);
+        },
+      });
+    },
+  },
   methods: {
     init() {
       let container = document.getElementById("container");
@@ -27,24 +49,34 @@ export default {
       this.sizes.width = window.innerWidth;
       this.sizes.height = window.innerWidth;
 
-      this.camera = new Three.PerspectiveCamera(70, this.sizes.width / this.sizes.height, 0.01, 10);
-      this.camera.position.z = 1;
+      this.camera = new THREE.PerspectiveCamera(
+        70,
+        this.sizes.width / this.sizes.height,
+        0.01,
+        10
+      );
+      this.camera.position.z = 4;
 
-      this.scene = new Three.Scene();
+      this.scene = new THREE.Scene();
 
-      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-      let material = new Three.MeshNormalMaterial();
+      this.controls = new OrbitControls(this.camera, container);
+      // this.controls.minPolarAngle = Math.PI / 4;
+      // this.controls.maxPolarAngle = Math.PI / 2 + Math.PI / 4;
+      // this.controls.enableDamping = true;
 
-      this.mesh = new Three.Mesh(geometry, material);
+      let geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+      let material = new THREE.MeshNormalMaterial();
+
+      this.mesh = new THREE.Mesh(geometry, material);
       this.scene.add(this.mesh);
 
-      this.renderer = new Three.WebGLRenderer({ antialias: true });
+      this.renderer = new THREE.WebGLRenderer({ antialias: true });
       this.renderer.setSize(this.sizes.width, this.sizes.height);
       this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
       container.appendChild(this.renderer.domElement);
     },
     resizeCanvas() {
-      console.log('resize')
+      console.log("resize");
       // Update sizes
       this.sizes.width = window.innerWidth;
       this.sizes.height = window.innerHeight;
@@ -70,7 +102,7 @@ export default {
       window.addEventListener("resize", this.resizeCanvas);
     });
 
-    this.resizeCanvas()
+    this.resizeCanvas();
   },
 };
 </script>
